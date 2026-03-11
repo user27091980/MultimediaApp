f42b5b832ccf22075db865577eba08d3c7d26070
Andrés <acanabalponcela@gmail.com>
2026-03-11 20:12:20 +0100

    no muestra imágenes

--- app/src/main/java/com/example/multimediaapp/viewmodel/vm/MainVM.kt
+++ app/src/main/java/com/example/multimediaapp/viewmodel/vm/MainVM.kt
@@ -2,6 +2,8 @@ package com.example.multimediaapp.viewmodel.vm
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.multimediaapp.data.repository.MainRepo
+import com.example.multimediaapp.network.ApiService
 import com.example.multimediaapp.viewmodel.uistate.MainUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -9,7 +11,8 @@ import kotlinx.coroutines.launch
 
 class MainVM : ViewModel() {
 
-    private val repo = MainRepoRetrofit()
+    // Creamos la instancia de ApiService y la pasamos al repositorio
+    private val repo = MainRepo(ApiService.create())
 
     private val _uiState = MutableStateFlow(MainUiState())
     val uiState: StateFlow<MainUiState> = _uiState
