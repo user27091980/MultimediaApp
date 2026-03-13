70cecdedf9cdee960abeb0fe19bc00ce4bb5263f
Andrés <acanabalponcela@gmail.com>
2026-03-13 16:47:43 +0100

    añadido logout, no cargan imágenes, falta usuarios

--- app/src/main/java/com/example/multimediaapp/view/components/LazyColumnComponent.kt
+++ app/src/main/java/com/example/multimediaapp/view/components/LazyColumnComponent.kt
@@ -1,7 +1,6 @@
 package com.example.multimediaapp.view.components
 
 import android.util.Log
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -26,7 +25,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.example.multimediaapp.model.MainDTO
 
