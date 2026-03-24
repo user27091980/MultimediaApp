1f924e31b069b62ff76c53d9a5eda4c559561d8e
Andrés <acanabalponcela@gmail.com>
2026-03-24 15:01:19 +0100

    cambio frontEnd, falta gestión usuarios, y dialogo modal

--- app/src/main/java/com/example/multimediaapp/view/pages/LoginRegPage.kt
+++ app/src/main/java/com/example/multimediaapp/view/pages/LoginRegPage.kt
@@ -1,19 +1,15 @@
 package com.example.multimediaapp.view.pages
 
-import com.example.multimediaapp.view.components.ButtonLogin
-import com.example.multimediaapp.view.components.ButtonRegister
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -21,6 +17,8 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.multimediaapp.navigation.ObjRoutes
 import com.example.multimediaapp.ui.theme.boxModifier
+import com.example.multimediaapp.view.components.ButtonLogin
+import com.example.multimediaapp.view.components.ButtonRegister
 import com.example.multimediaapp.viewmodel.vm.LoginRegVM
 
 /**
@@ -40,7 +38,8 @@ fun LoginRegScreen(
         modifier = boxModifier
             .fillMaxSize()
             .background(
-                androidx.compose.material3.MaterialTheme.colorScheme.background
+
+                MaterialTheme.colorScheme.background
             ),
         contentAlignment = Alignment.Center
     ) {
